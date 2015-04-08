4763646fa5ba3b7e5dd501b2c10d87933d6d2575
acotall <acotall510@gmail.com>
2015-04-08 19:25:46 +0200

    Ajout du message 'Merci OpenClassrooums' dans programme.py

--- programme.py
+++ programme.py
@@ -2,4 +2,7 @@
 
 """Ce fichier affiche simplement une ligne grâce à la fonction print."""
 
-print("\n Salut Bande de Zéros, Bonne continuation dans votre cours sur Git & Github! \n")
+print("\n \n Salut Bande de Zéros, Bonne continuation dans votre cours sur Git & Github! \n")
+print("\n >>>>>>>>>>>>>>>>>>>>>>>>>>> Merci OpenClassrooums <<<<<<<<<<<<<<<<<<<<<<<<< \n \n")
+
+
